3757fc418aee9d25ffcc5aae8c3f52c1c221afa5
Don Joseph <djoseph@opentext.com>
2025-09-29 15:09:57 -0400

    use axios to get data from backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,8 +3,14 @@ import "./HomePage.css";
 import Header from "../components/Header";
 import { products } from "../starting-code/data/products";
 import ProductContainer from "../components/ProductContainer";
+import axios from 'axios';
+
 
 function HomePage() {
+  axios.get('http://localhost:3000/api/products').then((response)=>{
+    console.log(response.data)
+  })
+
   return (
     <>
       <title>Ecommerce Project</title>
