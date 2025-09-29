e5464d4c3ad2086e77dd9ca455c74fc5a877e041
Don Joseph <djoseph@opentext.com>
2025-09-29 12:14:21 -0400

    update links to match router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from 'react-router';
 import './App.css'
 import HomePage from './pages/HomePage'
-
+import CheckoutPage from './pages/CheckoutPage';
 function App() {
 
   return (
     <Routes>
       <Route index element={<HomePage />} />
-      <Route path="checkout" element={<Checkout />} />
+      <Route path="checkout" element={<CheckoutPage />} />
     </Routes>
     
   )
